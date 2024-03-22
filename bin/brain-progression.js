ef1617b5e8d865ed106e0a1d3e7f14aa01b2b030
keronjenos@gmail.com <keronjenos@gmail.com>
2024-03-22 15:54:42 +0300

    new games

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,14 +1,29 @@
 #!/usr/bin/env node
-import { getRandomNum, doGameLogic } from '../src/index.js';
+import { doGameLogic, getRandomNum } from '../src/index.js';
 
 const rules = 'What number is missing in the progression?';
 
-const makeProgression = (firstNum, ) => {
-  
-}
+const makeProgression = () => {
+  const arr = [];
+  const firstNum = getRandomNum();
+  const minLen = Math.floor(Math.random() * 11);
+  const step = Math.floor(Math.random() * 6);
+
+  for (let i = firstNum, y = 0; y < minLen; i += step, y += 1) {
+    arr.push(i);
+  }
+
+  return arr;
+};
 
 const makeRound = () => {
-  
+  const arr = makeProgression();
+  const hiddenIndex = Math.floor(Math.random() * arr.length) - 1;
+
+  const correctAnswer = arr[hiddenIndex].toString();
+  arr[hiddenIndex] = '..';
+  const question = arr.join(' ');
+  return [question, correctAnswer];
 };
 
 const launchBrainProgression = () => {
